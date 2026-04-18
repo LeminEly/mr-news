0d50a5bf0580cb69b8af3f3d3418656da5c44e0a
chighali-hub <24013@supnum.mr>
2026-04-18 15:25:36 +0000

    commencou che9le

---
+++ mr_news/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("chet9el")),
+        body: Center(child: Text("commencou chte9lou")),
+      ),
+    );
+  }
+}
